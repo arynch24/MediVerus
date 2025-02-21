48b903bebb9b8d2c5523443552fc3f1c5a6572d6
Shoyeb45 <shoyebff45@gmail.com>
2025-02-21 18:17:36 +0530

    Added confidence score in response

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,11 +4,22 @@ from rest_framework.response import Response
 from rest_framework import status
 import pickle as pkl
 from util.process_text import process_text
+import numpy as np
 
 # Open model
 with open('backend/model.pkl', 'rb') as file:
     model = pkl.load(file)
 
+
+def getConfidenceScore(vectorized_input_data):
+    decision_score = model.decision_function(vectorized_input_data)[0]
+    
+    confidence = 1 / (1 + np.exp(-decision_score))
+    
+    fake_confidence = confidence * 100 if decision_score > 0 else (1 - confidence) * 100
+    real_confidence = (1 - confidence) * 100 if decision_score > 0 else confidence * 100
+    
+    return (fake_confidence, real_confidence)
 
 # Create your views here.
 @api_view(["POST"])
@@ -37,16 +48,18 @@ def predict(request):
         
         try:
             prediction = model.predict(vectorized_input_data)
+            fake_score, real_score = getConfidenceScore(vectorized_input_data)
         except Exception as e:
                 return Response(
                     {"error": f"Model prediction error: {str(e)}"},
                     status=status.HTTP_500_INTERNAL_SERVER_ERROR
                 )
         
-        
         return Response({
                 "input_data": input_data, 
-                "isFakeNews": prediction[0]
+                "isFakeNews": prediction[0],
+                "confidenceScoreOfFake": fake_score,
+                "confidenceScoreOfReal": real_score
             }, status = status.HTTP_200_OK)
         
     except Exception as e:
